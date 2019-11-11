39c4cdb1f9aa9ddb477f08de5b8b96aaf872ab76
NicPit11 <56974633+NicPit11@users.noreply.github.com>
2019-11-11 21:23:45 +0100

    Add files via upload

--- MathDialog.java
+++ MathDialog.java
@@ -1,110 +1,123 @@
-package OwnMath;
+
 
 import java.util.Scanner;
 import java.util.InputMismatchException;
 
 /**
- * Beschreiben Sie hier die Klasse MathDialog.
- * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
+ *Eine Dialogklasse zum Testen der Methoden(berechneTeilersumme/BerechneCheckIsbn/berechneNullstellen).
+ *Soll interativ genutzt werden können und bei falschen Eingaben eine sinnvolle Aussage anzeigen 
+ *aus welchem Grund diese Eingabe nicht gültig war. 
+ * @author Oliver Theobald
+ * @author Nicolas Schnur
+ * @version (1.0)
  */
-public class MathDialog  
-{
+public class MathDialog extends MathFunctions  {
+   private Scanner input = new Scanner(System.in);
+   MathFunctions rechnung = new MathFunctions();
+ 
+   private static final int TEILSUMME= 1;
+   private static final int PRUEFZIFFER = 2; 
+   private static final int NULLSTELLE = 3;
+   private static final int BEENDEN = 0;
 
- //private berechneTeilSumme Mathfunktions01;
- private Scanner input = new Scanner(System.in);
-    
-    
- private static final int TEILSUMME= 1;
- private static final int PRUEFZIFFER = 2; 
- private static final int NULLSTELLE = 3;
- private static final int BEENDEN = 0;
-
- private int einlesenFunktion() {
- System.out.println("Was wollen sie berechen ?");
- System.out.println();
- System.out.print(TEILSUMME + ":Teilsumme ; "+
-                  PRUEFZIFFER+ ":Prüfziffer ; "+
-                  NULLSTELLE+ ":Nullstelle ; "+
-                  BEENDEN+ " :Beenden-> ");
-                  return input.nextInt();
- }
-
- private void  ausfuehrenFunktion(int funktion) {
-     long zahl;
-     double p;
-     double q;
-     long isbn;
-     MathFunctions summe = new MathFunctions();
-     
-        if (funktion == TEILSUMME) { 
-            System.out.println("Positive Ganze Zahl eingeben : ");
-            zahl = input.nextLong();
-            System.out.println(summe.berechneTeilSumme(zahl));
-            
-            
-             
-             
-        }else if (funktion == PRUEFZIFFER) {
-            System.out.println("9-stellige ISBN eingeben");
-            isbn = input.nextLong();
-            System.out.println(summe.berechneChecksummeIsbn(isbn));
-                   
-            
-        }else if (funktion == NULLSTELLE) {
-            System.out.println("p und q bitte eingeben ");
-            System.out.println("p ? ");
-            p = input.nextDouble();
-            System.out.println("q ? ");
-            q = input.nextDouble();
-            System.out.println(summe.berechneNullstelle (p,q));
-                  
-        }else if (funktion == BEENDEN) {
-                  
-        }else {
-                 System.out.println("Falsche Eingabe!");
-        }
+   /**
+    *Abbildung des Menues und Eingabe von auszufuehrender Methode. 
+    */
+   private int einlesenFunktion() {
+        System.out.println("Was wollen Sie berechnen ?");
         System.out.println();
-    }
-    
-    
-    
-    
-    
-    
+        System.out.print(TEILSUMME + ":Teilsumme ; "+
+        PRUEFZIFFER+ ":Pruefziffer ; "+
+        NULLSTELLE+ ":Nullstelle ; "+
+        BEENDEN+ " :Beenden-> ");
  
- 
- 
- 
- 
- 
+        return input.nextInt();
+   }
 
-public void start() {
-         int funktion = -1;
-        while (funktion != BEENDEN) {
-            try {
+   /**
+    * Verknuepfung der Eingabe des Benutzers mit den verschiedenen Methoden.
+    */
+   private void  ausfuehrenFunktion(int funktion) {
+        if (funktion == TEILSUMME) { 
+            berechneTeilersumme();
+        }else if (funktion == PRUEFZIFFER) {
+            berechneChecksummeIsbn();
+        }else if (funktion == NULLSTELLE) {
+            berechneNullstellen();
+        }else if (funktion == BEENDEN) {
+            System.out.println("Programmende");     
+        }else {
+            System.out.println("Falsche Eingabe!");
+        }
+   }
+    
+   /**
+    * Methode greift auf auf die Methode zur Berechnung einer Teilersumme zu und gibt
+    * den erechneten Wert  aus .
+    *
+    */
+   private void berechneTeilersumme(){
+       long zahl;
+       System.out.print("Positive Ganze Zahl eingeben : ");
+       zahl = input.nextLong();
+            System.out.println(rechnung.berechneTeilSumme(zahl)); 
+    
+   }
+   
+   /**
+   * Methode greift auf auf die Methode zur Berechnung einer Pruefziffer zu und gibt
+   * den erechneten Wert  aus.
+   */
+   private void berechneChecksummeIsbn(){
+        long isbn;
+        System.out.println("9-stellige ISBN eingeben");
+        isbn = input.nextLong();
+        System.out.println(rechnung.berechneChecksummeIsbn(isbn));
+    
+   }
+   
+    /**
+     * Methode greift auf auf die Methode zur Berechnung von Nullstellen zu und gibt
+     * die erechneten Werte  aus.
+     */
+    private void berechneNullstellen() {
+        double p;
+        double q;
+        System.out.println("p und q bitte eingeben ");
+        System.out.print("p = ");
+        p = input.nextDouble();
+        System.out.print("q = ");
+        q = input.nextDouble();
+        System.out.println(rechnung.berechneNullstelle (p,q));
+   }
+    
+   /**
+   * Hauptschleife des Dialogs
+   * ermoeglicht die wiederholte Eingabe von auszufuehrenden Methoden
+   * und faengt nicht zulässige Eingaben ab.
+   */
+   public void start() {
+       int funktion = -1;
+       while (funktion != BEENDEN) {
+           try {
                 funktion = einlesenFunktion();
                 ausfuehrenFunktion(funktion);
-            
-            }catch (IllegalArgumentException e) {
+           }catch (IllegalArgumentException e) {
                 System.out.println(e);
-            }catch (InputMismatchException e) {
+           }catch (InputMismatchException e) {
                 System.out.println(e); 
                 input.nextLine();
-            }catch (Exception e) {
+           }catch (Exception e) {
                 System.out.println("Ausnahme gefangen: " + e);
                 e.printStackTrace(System.out);
-            }          
+           }          
         }
-    }  
+   }  
  
- 
- 
- public static void main (String[] args) {
-        new MathDialog().start();
-
-
-
-}
+   /**
+    * Methode startet den Dialog und die Hauptschleife. 
+    */
+   public static void main (String[] args) {
+       new MathDialog().start();
+   }
 }
--- MathFunctions.java
+++ MathFunctions.java
@@ -1,72 +1,80 @@
-
-package OwnMath;
-
 import java.lang.Math;
 import java.util.Scanner;
 /**
- * Beschreiben Sie hier die Klasse MathFunctions.
- * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
+ * Klasse mit Methoden in welchen Formeln , fuer verschiedene mathematische Rechnungen, programmiert sind.
+ * @author  Oliver Theobald
+ * @author Nicolas Schnur
+ * @version (1.0)
  */
 public class MathFunctions {
     private Scanner input = new Scanner(System.in);
     private static final long MAX =1000000000;
     private static final long MIN =100000000;
+    /** Methode zum berechnen der Teilersumme.
+    * Hier wird die eingegebene Zahl durch jede kleinere Zahl geteilt und daraus die Summe gebildet.
+    */ 
     public static long berechneTeilSumme(long zahl) {
-       if (zahl<0){
-       throw new IllegalArgumentException("es dürfen nur natürliche Zahlen"+
-       "eingegeben werden ");}
-    long summe = 0;
+        if (zahl<0){
+            throw new IllegalArgumentException("es dürfen nur natürliche Zahlen"+
+            "eingegeben werden ");}
+        if (zahl ==0) {
+            throw new IllegalArgumentException("die Zahl 0 hat keine Teiler");  }
+        long summe = 0;
         for (long i=1; i <=zahl; i++) {
-            if(zahl % i==0) {
-                summe = summe + i;
+          if(zahl % i==0) { 
+             summe = summe + i;
+          }
         }
-      }
-    return summe;
-   }
-
-   public static String berechneChecksummeIsbn( long isbn){
-        if( isbn<=MIN ||isbn>MAX){
-               throw new IllegalArgumentException(
-               "Zahl ist nicht 9-stellig"); }
-      long summe= 0;
-      long pruefziffer= 0;
-      long isbn1=0;
-       
-        while(isbn>0){
-              for (long i=9; i>0; i--) {
-              isbn1 = isbn1 + (isbn % 10) * i;
-              isbn= isbn/10 ;
-              summe = isbn1;
-              }
-        }
-      pruefziffer = summe % 11;
-      if(pruefziffer == 10) {
-          return "Pruefziffer : X mod 11";
-        }
-      return "Pruefziffer: "+pruefziffer +" mod 11";
-  }
-    
-   public static String berechneNullstelle (double p, double q) {
-    double x1 = 0;
-    double x2 = 0;
-    String Ergebnis;
-    x1 = -p/2 +  Math.sqrt(Math.pow(p/2,2)-q);
-    x2 = -p/2 - Math.sqrt(Math.pow(p/2,2)-q);
-    if (Math.sqrt(Math.pow(p/2,2)-q) >0) {
-        System.out.println();
-        Ergebnis="Zwei Nullstellen : "+ x1 +" | "+ x2; 
-    }else if (Math.sqrt(Math.pow(p/2,2)-q) ==0){
-        System.out.println();
-        Ergebnis = "Doppelte Nullstelle : "+ x1;
-    }else{
-        System.out.println();
-        Ergebnis = "Komplexe Nullstellen";
+        return summe;
     } 
-    return Ergebnis;
-  }
-  
+    
+    /**Methode zum berechnen der Pruefziffer einer 10-stelligen ISBN.
+     * Hier wird die ISBN in ihre ersten neun Ziffern zerlegt und mit den Zahlen von 1-9 multipliziert und anschließend die Summe gebildet.
+     * Das Ergebnis wird mit mod 11 verrechnet um die Pruefziffer zu erhalten.
+     */
+    public static String berechneChecksummeIsbn( long isbn){
+        if( isbn<=MIN ||isbn>MAX){
+            throw new IllegalArgumentException(
+            "Zahl ist nicht 9-stellig"); }
+        long summe= 0;
+        long pruefziffer= 0;
+        long isbn1=0;
+        while(isbn>0){
+            for (long i=9; i>0; i--) {
+             isbn1 = isbn1 + (isbn % 10) * i;
+             isbn= isbn/10 ;
+             summe = isbn1;
+            }
+        }
+        pruefziffer = summe % 11;
+        if(pruefziffer == 10) {
+            return "Pruefziffer : X mod 11";
+        }
+        return "Pruefziffer: "+pruefziffer +" mod 11";
+    }
+    
+    /**Methode zur berechnung der Nullstellen einer quadratischen Funktion .
+     * Hierzu wird die Pq-Formel angewendet.
+     * Dann wird die Art der Nullstelle mit ihren Werten ausgegeben.
+     */
+    public static String berechneNullstelle (double p, double q) {
+        double x1 = 0;
+        double x2 = 0;
+        String Ergebnis;
+        x1 = -p/2 +  Math.sqrt(Math.pow(p/2,2)-q);
+        x2 = -p/2 - Math.sqrt(Math.pow(p/2,2)-q);
+        if (Math.sqrt(Math.pow(p/2,2)-q) >0) {
+            System.out.println();
+            Ergebnis="Zwei Nullstellen : "+ x1 +" | "+ x2; 
+        }else if (Math.sqrt(Math.pow(p/2,2)-q) ==0){
+           System.out.println();
+           Ergebnis = "Doppelte Nullstelle : "+ x1;
+        }else{
+           System.out.println();
+           Ergebnis = "Komplexe Nullstellen";
+        } 
+        return Ergebnis; 
+    }
 }
     
 
